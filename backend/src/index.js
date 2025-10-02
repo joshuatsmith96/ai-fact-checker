95fdf7e28876a576b649d4a50bb89c6b39febefb
joshuatsmith96 <joshuatsmith96@gmail.com>
2025-10-02 18:06:36 -0400

    Removed HOST

--- backend/src/index.js
+++ backend/src/index.js
@@ -59,5 +59,5 @@ app.post("/api/gemini", async (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
