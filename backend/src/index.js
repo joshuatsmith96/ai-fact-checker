fd11d4db88a4ff225dde43a6d398e99c914562cf
joshuatsmith96 <joshuatsmith96@gmail.com>
2025-10-02 18:04:51 -0400

    Removed unecessary variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -56,9 +56,8 @@ app.post("/api/gemini", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT;
-const HOST = process.env.HOST;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
   console.log(`Server running on http://${HOST}:${PORT}`);
 });
